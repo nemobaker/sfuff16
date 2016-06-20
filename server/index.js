41d5322d0794988350a5bab8d852ae32e96d738e
Kim Curran <currankim7@gmail.com>
2016-06-19 23:47:35 -0700

    Create basic express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-//initialize server
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('Testing');
+});
+
+app.listen(3000, function() {
+  console.log('Listening on Port 3000');
+});
