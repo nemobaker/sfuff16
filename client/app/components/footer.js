2d6a9193220f64097c39bf4ad422fa4d2131718b
Kim Curran <currankim7@gmail.com>
2016-06-21 12:48:47 -0700

    Update footer component

--- client/app/components/footer.js
+++ client/app/components/footer.js
@@ -1,1 +1,22 @@
-//footer.js
+import React from 'react';
+
+class Footer extends React.Component {
+  constructor(props) {
+  	super(props);
+  }
+
+  render() {
+  	return (
+      <footer>
+        <ul>
+          <li>patreon</li>
+          <li>facebook</li>
+          <li>instagram</li>
+          <li>twitter</li>
+        </ul>
+      </footer>
+  	);
+  }
+}
+
+export default Footer;
