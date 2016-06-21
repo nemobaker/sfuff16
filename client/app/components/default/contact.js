1fe65e5e9ec42d364a04e10b819b7f31b45fe7ba
nemobaker <nemobaker@gmail.com>
2016-06-21 16:46:01 -0700

    add state to contact component

--- client/app/components/default/contact.js
+++ client/app/components/default/contact.js
@@ -1,44 +1,51 @@
 import React from 'react';
 import $ from 'jquery';
+import _ from 'underscore';
 
 class Contact extends React.Component {
   constructor(props) {
   	super(props);
 
   	this.state = {
-      name: '',
-      email: '',
-      message: ''
+      name: 'nemo',
+      email: null,
+      message: null,
     };
   }
 
-  let sendEmail =     $.post('http://localhost:3000/send', {
-      name: this.state.name,
-      email: this.state.email,
-      message: this.state.message
-    })
+  changeName(e) {
+    this.setState({
+      name: e.target.value,
+    });
+  }
+
+  sendEmail(){
+    $.post('http://localhost:3000/send', {
+        name: this.state.name,
+        email: this.state.email,
+        message: this.state.message
+      })
+  }
 
   render() {
     return (
       <div>
         <h2>contact</h2>
-        <form>
           <div>
-            <label for="name">Name:</label>
-            <input type="text" id="name" />
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" value={this.state.name} onChange={this.changeName.bind(this)} />
           </div>
           <div>
-            <label for="email">Email:</label>
-            <input type="email" id="email" />
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" value={this.state.name} onChange={console.log('clicked')} />
           </div>
           <div>
-            <label for="message">Message:</label>
+            <label htmlFor="message">Message:</label>
             <input type="text" id="message" />
           </div>
           <div className="button">
-            <button type="submit">Submit</button>
+            <button onClick={this.sendEmail.bind(this)}>Submit</button>
           </div>
-        </form>
       </div>
     );
   }
