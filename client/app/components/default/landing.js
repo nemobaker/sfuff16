2c8e8339fe2a4203dd339f52411911be37089c46
Kim Curran <currankim7@gmail.com>
2016-06-21 12:37:06 -0700

    Update Landing component

--- client/app/components/default/landing.js
+++ client/app/components/default/landing.js
@@ -1,1 +1,20 @@
-//landing.js
+import React from 'react';
+
+class Landing extends React.Component {
+  constructor(props) {
+  	super(props);
+
+  	this.state = {};
+  }
+
+  render() {
+    return() {
+      <div>
+        <h1>sf urban film fest</h1>
+        <h4>subtext</h4>
+      </div>
+    }
+  }
+}
+
+export default Landing;
