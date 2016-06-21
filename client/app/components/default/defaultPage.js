6b5118b97cd7c33e6e52ace1d7ec8e058539aee6
Kim Curran <currankim7@gmail.com>
2016-06-21 12:15:48 -0700

    Update defaultPage component

--- client/app/components/default/defaultPage.js
+++ client/app/components/default/defaultPage.js
@@ -1,1 +1,22 @@
-//defaultPage.js
+import React from 'react';
+
+class Default extends React.Component {
+  constructor (props) {
+  	super(props);
+
+  	this.state = {};
+  }
+
+  render() {
+  	return (
+      <div>
+        <Landing />
+        <About />
+        <Patreon />
+        <Contact />
+      </div>
+  	)
+  }
+}
+
+export default Default;
