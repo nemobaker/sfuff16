ae2a09c716da22953e4781b483d39ff62f86ed9b
Kim Curran <currankim7@gmail.com>
2016-06-21 12:05:34 -0700

    Add defaultPage component, update App.js with state-based viewing

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,51 @@
-//app.js
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+
+class App extends React.Component {
+  constructor (props) {
+  	super(props);
+
+  	this.state = {
+  	  page: 'default',
+  	};
+
+    this.patreon = this.patreon.bind(this);
+  }
+
+  patreon() {
+  	return this.setState({
+  	  page: 'patreon',
+  	});
+  }
+
+  render() {
+  	let pageLayout;
+
+  	if (this.state.page === 'default') {
+  	  pageLayout = (
+        <div>
+          <Default />
+        </div>
+  	  )
+  	}
+
+  	if (this.state.page === 'patreon') {
+  	  pageLayout = (
+        <div>
+          Patreon View
+        </div>
+  	  )
+  	}
+
+  	return (
+      <div>
+        <Header />
+          {pageLayout}
+        <Footer />
+      </div>
+  	);
+  }
+}
+
+export default App;
