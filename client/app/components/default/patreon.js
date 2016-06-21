8535503ce52fc6c961064ce21924ac00b9258846
Kim Curran <currankim7@gmail.com>
2016-06-21 12:40:02 -0700

    Update Patreon component

--- client/app/components/default/patreon.js
+++ client/app/components/default/patreon.js
@@ -1,1 +1,20 @@
-//patreon.js
+import React from 'react';
+
+class Patreon extends React.Component {
+  constructor(props) {
+  	super(props);
+
+  	this.state = {};
+  }
+
+  render() {
+    return() {
+      <div>
+        <h2>patreon</h2>
+        <h4>patreon info here</h4>
+      </div>
+    }
+  }
+}
+
+export default Patreon;
