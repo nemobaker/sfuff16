e94a271e8f7e2afa1abc66743bca0c553872b6de
Kim Curran <currankim7@gmail.com>
2016-06-21 12:46:21 -0700

    Update header component

--- client/app/components/header.js
+++ client/app/components/header.js
@@ -1,1 +1,24 @@
-//header.js
+import React from 'react';
+
+class Header extends React.Component {
+  constructor(props) {
+  	super(props);
+
+  	this.state = {}
+  }
+
+  render() {
+  	return (
+      <div>
+        <h2>sf urban film fest</h2>
+        <ul>
+          <li>about</li>
+          <li>patreon</li>
+          <li>contact</li>
+        </ul>
+      </div>
+  	);
+  }
+}
+
+export default Header;
