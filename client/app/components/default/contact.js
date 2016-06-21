118be560e0747f056ff99a7297536922ceb3670c
Kim Curran <currankim7@gmail.com>
2016-06-21 12:30:03 -0700

    Update Contact component

--- client/app/components/default/contact.js
+++ client/app/components/default/contact.js
@@ -1,1 +1,36 @@
-//contact.js
+import React from 'react';
+
+class Contact extends React.Component {
+  constructor(props) {
+  	super(props);
+
+  	this.state = {};
+  }
+
+  render() {
+    return() {
+      <div>
+        <h2>contact</h2>
+        <form>
+          <div>
+            <label for="name">Name:</label>
+            <input type="text" id="name" />
+          </div>
+          <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" />
+          </div>
+          <div>
+            <label for="message">Message:</label>
+            <input type="text" id="message" />
+          </div>
+          <div className="button">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    }
+  }
+}
+
+export default Contact;
