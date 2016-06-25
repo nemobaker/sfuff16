c4adf2894cb8bbfbf9f125ab7eb43ee05b8c54ec
nemobaker <nemobaker@gmail.com>
2016-06-25 16:21:52 -0700

    add functions to update state and send contact information

--- client/app/components/default/contact.js
+++ client/app/components/default/contact.js
@@ -14,18 +14,18 @@ class Contact extends React.Component {
         <form>
           <div>
             <label htmlFor="name">Name:</label>
-            <input type="text" id="name" />
+            <input type="text" id="name" value={this.props.updateName()} onChange={(e) => this.props.updateName(e)} />
           </div>
           <div>
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" />
+            <input type="email" id="email" value={this.props.updateEmail()} onChange={(e) => this.props.updateEmail(e)} />
           </div>
           <div>
             <label htmlFor="message">Message:</label>
-            <input type="text" id="message" />
+            <input type="text" id="message" value={this.props.updateMessage()} onChange={(e) => this.props.updateMessage(e)} />
           </div>
           <div className="button">
-            <button type="submit">Submit</button>
+            <button type="button" onClick={(e) => this.props.handleContactForm(e)}>Submit</button>
           </div>
         </form>
       </div>
