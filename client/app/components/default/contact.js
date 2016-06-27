9ee7ba32672753b2592ecc72393783f243f7c146
Kim Curran <currankim7@gmail.com>
2016-06-26 22:22:49 -0700

    Update contact section styling

--- client/app/components/default/contact.js
+++ client/app/components/default/contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class Contact extends React.Component {
   constructor(props) {
@@ -10,24 +11,57 @@ class Contact extends React.Component {
   render() {
     return (
       <div className="contact-section">
-        <h2>contact</h2>
-        <form>
-          <div>
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" value={this.props.updateName()} onChange={(e) => this.props.updateName(e)} />
-          </div>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" value={this.props.updateEmail()} onChange={(e) => this.props.updateEmail(e)} />
-          </div>
-          <div>
-            <label htmlFor="message">Message:</label>
-            <input type="text" id="message" value={this.props.updateMessage()} onChange={(e) => this.props.updateMessage(e)} />
-          </div>
-          <div className="button">
-            <button type="button" onClick={(e) => this.props.handleContactForm(e)}>Submit</button>
-          </div>
-        </form>
+        <div className="contact-container">
+          <Grid>
+            <Row className="show-grid">
+              <Col md={2}>
+                <h2>
+                  <span>contact</span>
+                </h2>
+              </Col>
+              <Col md={1} />
+              <Col md={8}>
+                <form>
+                  <div>
+                    <label htmlFor="name">Name:</label>
+                    <input
+                      type="text"
+                      id="name"
+                      value={this.props.updateName()}
+                      onChange={(e) => this.props.updateName(e)}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                      type="email"
+                      id="email"
+                      value={this.props.updateEmail()}
+                      onChange={(e) => this.props.updateEmail(e)}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="message">Message:</label>
+                    <input
+                      type="text"
+                      id="message"
+                      value={this.props.updateMessage()}
+                      onChange={(e) => this.props.updateMessage(e)}
+                    />
+                  </div>
+                  <div className="button">
+                    <button
+                      type="button"
+                      onClick={(e) => this.props.handleContactForm(e)}
+                    >
+                    Submit
+                    </button>
+                  </div>
+                </form>
+              </Col>
+            </Row>
+          </Grid>
+        </div>
       </div>
     );
   }
