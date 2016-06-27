a1e0dc68b7c01817c875cd697b0f39c534578bc7
Kim Curran <currankim7@gmail.com>
2016-06-26 21:54:33 -0700

    Add in festival dates, fix about section styling

--- client/app/components/default/about.js
+++ client/app/components/default/about.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class About extends React.Component {
   constructor(props) {
@@ -10,8 +11,24 @@ class About extends React.Component {
   render() {
     return (
       <div className="about-section">
-        <h2>about</h2>
-        <h4>about info here</h4>
+        <div className="about-container">
+          <Grid>
+            <Row className="show-grid">
+              <Col md={2}>
+                <h2>
+                  <span>about</span>
+                </h2>
+              </Col>
+              <Col md={1} />
+              <Col md={8}>
+                <h4>The SF Urban Film Fest (SFUFF) is a unique film festival that focuses on cities and civic engagement inspired by great storytelling.  The festival believes compelling stories can help shape urbanist ideas, practice, and project implementation to improve our cities.</h4>
+                <br />
+                <h4>SFUFF is organized by urban planners and screens films in tandem with panel discussions and storytelling workshops.   Our 3rd annual week-long festival will be November 14-20, 2016.</h4>
+              </Col>
+              <Col md={1} />
+            </Row>
+          </Grid>
+        </div>
       </div>
     );
   }
