82930895893e8f260f22bde40d458a15fd3c2863
Kim Curran <currankim7@gmail.com>
2016-06-28 10:21:29 -0700

    Tweak mobile view styling, edit about section

--- client/app/components/default/about.js
+++ client/app/components/default/about.js
@@ -22,7 +22,6 @@ class About extends React.Component {
               <Col md={1} />
               <Col md={8}>
                 <h4>The SF Urban Film Fest (SFUFF) is a unique film festival that focuses on cities and civic engagement inspired by great storytelling.  The festival believes compelling stories can help shape urbanist ideas, practice, and project implementation to improve our cities.</h4>
-                <br />
                 <h4>SFUFF is organized by urban planners and screens films in tandem with panel discussions and storytelling workshops.   Our 3rd annual week-long festival will be November 14-20, 2016.</h4>
               </Col>
               <Col md={1} />
