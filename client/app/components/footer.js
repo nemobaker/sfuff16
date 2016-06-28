dd4aed6ca0d12d0f5fc2f12ae93d9fc969f1335b
Kim Curran <currankim7@gmail.com>
2016-06-28 14:38:56 -0700

    Update footer with social links

--- client/app/components/footer.js
+++ client/app/components/footer.js
@@ -11,16 +11,16 @@ class Footer extends React.Component {
         <div className="container">
           <ul className="social-icons">
             <li>
-              <a href="#"><i className="fa fa-facebook fa-2x"></i></a>
+              <a href="https://www.facebook.com/sfurbanfilmfest/"><i className="fa fa-facebook fa-2x"></i></a>
             </li>
             <li>
-              <a href="#"><i className="fa fa-instagram fa-2x"></i></a>
+              <a href="https://www.instagram.com/sfurbanfilmfest/"><i className="fa fa-instagram fa-2x"></i></a>
             </li>
             <li>
-              <a href="#"><i className="fa fa-twitter fa-2x"></i></a>
+              <a href="https://twitter.com/sfurbanfilmfest"><i className="fa fa-twitter fa-2x"></i></a>
             </li>
             <li>
-              <a href="#"><i className="fa fa-youtube fa-2x"></i></a>
+              <a href="https://www.youtube.com/channel/UCa0T4tQBqEqVx-OApGF0QKg/feed"><i className="fa fa-youtube fa-2x"></i></a>
             </li>
           </ul>
           <h5>© SF Urban Film Fest 2016</h5>
