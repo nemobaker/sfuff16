dd042ea0f71229806f20544eac8c4fca4b438d68
Kim Curran <currankim7@gmail.com>
2016-06-29 13:51:47 -0700

    Add Patreon content to respective component

--- client/app/components/default/patreon.js
+++ client/app/components/default/patreon.js
@@ -21,7 +21,9 @@ class Patreon extends React.Component {
               </Col>
               <Col md={1} />
               <Col md={8}>
-                <h4>patreon info here</h4>
+                <h4>Patreon is a crowdfunding platform for artists. This year SFUFF will be featured on Patreon, allowing us to adopt a sustainable business model as we grow and also to expand our presence in the virtual world.</h4>
+                <h4>By joining our Patreon community and sponsoring SFUFF, receive exclusive access to festival updates, weekly behind-the-scenes content, a monthly Q&A session, and special film screenings. </h4>
+                <h4>For more information, visit our Patreon page here.</h4>
               </Col>
               <Col md={1} />
             </Row>
